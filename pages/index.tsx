e0b39c0b0e559e67482349d7f0dadcb1a5a81f8a
Noobosapien <migirulz44@gmail.com>
2024-05-29 19:43:55 +1200

    Learn 1

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,11 +13,7 @@ export default function Home() {
   return (
     <>
     <div className="bg" />
-      {/* <h1>
-        Learn <span style={{ fontSize: '0.4em' }}>with</span>
-        <br />
-        <span>Jason</span>
-      </h1> */}
+      
     <Canvas dpr={[1.5, 2]} linear shadows>
     <Learn/>
   </Canvas>
